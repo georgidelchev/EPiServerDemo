7a41218c15812b687a00f8b828ab60ad1c7deafd
Georgi Delchev <Georgi.Delchev@indeavr.com>
2021-08-06 14:45:39 +0300

    implement IPageViewModel and PageViewModel interface and class.

---
+++ EPiServerDemo/Models/ViewModels/IPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using EPiServer.Core;
+using EPiServerDemo.Models.Pages;
+
+namespace EPiServerDemo.Models.ViewModels
+{
+    public interface IPageViewModel<T>
+        where T : SitePageData
+    {
+        T CurrentPage { get; set; }
+
+        StartPage StartPage { get; set; }
+
+        IEnumerable<SitePageData> MenuPages { get; set; }
+
+        IContent Section { get; set; }
+    }
+}
