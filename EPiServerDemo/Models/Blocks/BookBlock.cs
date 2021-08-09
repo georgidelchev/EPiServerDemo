5e83fbd0cd83038e9b0a2a7578ad1c8690f602a4
Georgi Delchev <Georgi.Delchev@indeavr.com>
2021-08-09 16:03:18 +0300

    add book block

---
+++ EPiServerDemo/Models/Blocks/BookBlock.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EPiServerDemo.Models.Blocks
+{
+    [ContentType(
+        DisplayName = "BookBlock",
+        GUID = "afdc17aa-9b7a-43a8-8145-3321a9e15c28",
+        Description = "Block for creation of book.", Order = 200)]
+    public class BookBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(Name = "Book Heading",
+            Order = 100,
+            Description = "Book Heading",
+            GroupName = SystemTabNames.Content)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Book Description",
+            Order = 200,
+            Description = "Book Description",
+            GroupName = SystemTabNames.Content)]
+        public virtual XhtmlString Description { get; set; }
+
+        [Display(Name = "Book Image",
+            Order = 300,
+            Description = "Book Image",
+            GroupName = SystemTabNames.Content)]
+        public virtual Url Image { get; set; }
+
+        [Display(Name = "Book Url",
+            Order = 400,
+            Description = "Book Url",
+            GroupName = SystemTabNames.Content)]
+        public virtual string Url { get; set; }
+    }
+}
