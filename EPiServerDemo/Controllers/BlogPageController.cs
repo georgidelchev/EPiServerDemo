89087aceedb5e857fd7ebfc0369171184a4fb8ec
Georgi Delchev <Georgi.Delchev@indeavr.com>
2021-08-10 09:48:06 +0300

    add blog page

---
+++ EPiServerDemo/Controllers/BlogPageController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using EPiServer;
+using EPiServerDemo.Models.Pages;
+
+namespace EPiServerDemo.Controllers
+{
+    public class BlogPageController : PageControllerBase<BlogPage>
+    {
+        public BlogPageController(
+            IContentLoader loader) 
+            : base(loader)
+        {
+        }
+
+        public ActionResult Index(BlogPage currentPage)
+        {
+            return this.View(this.CreatePageViewModel(currentPage));
+        }
+    }
+}
---
+++ EPiServerDemo/Models/Pages/BlogPage.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServerDemo.Models.Blocks;
+
+namespace EPiServerDemo.Models.Pages
+{
+    [ContentType(
+        DisplayName = "Blog Page",
+        GUID = "f62c4295-f100-471e-9b63-f9594116b67f",
+        Description = "Blog PAGE")]
+    public class BlogPage : StandardPage
+    {
+        [Display(
+            Name = "Heading",
+            GroupName = SystemTabNames.Content,
+            Description = "Page Heading",
+            Order = 100)]
+        public virtual string Heading { get; set; }
+
+        [Display(
+            Name = "Main Body",
+            GroupName = SystemTabNames.Content,
+            Description = "Main Body",
+            Order = 200)]
+        public virtual XhtmlString MainBody { get; set; }
+
+        [Display(
+            Name = "Book",
+            GroupName = SystemTabNames.Content,
+            Description = "Page Book",
+            Order = 300)]
+        public virtual BookBlock Book { get; set; }
+    }
+}
